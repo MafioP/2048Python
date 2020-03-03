9def47a90de0892798ac372734e34a4ac8bb09f8
MafioP <42208309+MafioP@users.noreply.github.com>
2020-03-03 10:56:11 +0100

    boardPrinter

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("***********************2048**********************")
+
+def menu():
+    print("***********************2048**********************")
+    print("Emepezar nueva partida: 1")
+    print("Empezar partida guardada: 2")
+    print("Salir del juego: 3")
+    select = input("Seleccione una opcion: ")
+    return select
+
+def board(size, blocks, mode):
+    for i in range(size):
+        print("+", end="")
+        for j in range(size):
+            for l in range(mode):
+                print("-", end="")
+            print("+", end="")
+        print("")
+        for k in range(size):
+            print("|", end="")
+            for m in range(mode):
+                print(" ", end="")
+        print("|")
+    for j in range(size):
+        print("+", end="")
+        for k in range(mode):
+            print("-", end="")
+    print("+")
+
+print(menu())
+board(4, 0, 5)
